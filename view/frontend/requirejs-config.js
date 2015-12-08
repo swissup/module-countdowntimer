928639c77c20302f881a286c4c2767d4efbe2766
Vitaliy Dmytruk <vitaliy@mage2cloud.com>
2015-12-08 11:23:21 +0200

    js and css included

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+
+    map: {
+        '*': {
+            'flipclock' : 'Swissup_Countdowntimer/js/Swissup/flipclock/flipclock',
+            'simpletimer' : 'Swissup_Countdowntimer/js/Swissup/simpletimer/simpletimer'
+        }
+    },
+
+    shim : {
+        'flipclock' : ['jquery'],
+        'simpletimer' : ['jquery']
+    }
+
+};
