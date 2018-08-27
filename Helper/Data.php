e626eaf0c67c97047d21a1524df0805c9bde6c75
Vova Yatsyuk <vovayatsyuk@users.noreply.github.com>
2018-08-27 17:49:43 +0300

    Use scopeConfig from parent class

--- Helper/Data.php
+++ Helper/Data.php
@@ -9,12 +9,6 @@ use Magento\Store\Model\ScopeInterface;
  */
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
-
-    /**
-     * @var \Magento\Framework\App\Config\ScopeConfigInterface
-     */
-    protected $_scopeConfig;
-
     /**
      * @var \Magento\Framework\Locale\ResolverInterface
      */
@@ -23,23 +17,20 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
      * Constructor
      *
      * @param \Magento\Framework\App\Helper\Context $context
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
      * @SuppressWarnings(PHPMD.ExcessiveParameterList)
      */
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
-        \Magento\Framework\Locale\ResolverInterface $localeResolver,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        \Magento\Framework\Locale\ResolverInterface $localeResolver
     )
     {
         parent::__construct($context);
-        $this->_scopeConfig = $scopeConfig;
         $this->_localeResolver = $localeResolver;
     }
 
     protected function _getConfig($key)
     {
-        return $this->_scopeConfig->getValue($key, ScopeInterface::SCOPE_STORE);
+        return $this->scopeConfig->getValue($key, ScopeInterface::SCOPE_STORE);
     }
 
     public function getLocale()
