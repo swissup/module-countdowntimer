3e4f926436c58c93bdae360ca3da4bcedc29c87b
Alexander Kras'ko <0m3r.mail@gmail.com>
2019-05-29 14:53:58 +0300

    Fix js file location

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,9 +2,9 @@ var config = {
 
     map: {
         '*': {
-            'flipclock' : 'Swissup_Countdowntimer/js/swissup/countdowntimer/flipclock/flipclock',
-            'simpletimer' : 'Swissup_Countdowntimer/js/swissup/countdowntimer/simpletimer/simpletimer',
-            'starttimers' : 'Swissup_Countdowntimer/js/swissup/countdowntimer/starttimers'
+            'flipclock' : 'Swissup_Countdowntimer/js/Swissup/countdowntimer/flipclock/flipclock',
+            'simpletimer' : 'Swissup_Countdowntimer/js/Swissup/countdowntimer/simpletimer/simpletimer',
+            'starttimers' : 'Swissup_Countdowntimer/js/Swissup/countdowntimer/starttimers'
         }
     },
 
