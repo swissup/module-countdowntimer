c2afda1dd33c8a6261feabfdcd0dea181dbf2342
Vitaliy Dmytruk <vitaliy@mage2cloud.com>
2020-02-05 18:02:45 +0200

    Refactor require js config.

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,15 +1,6 @@
 var config = {
-
-    map: {
-        '*': {
-            'simpletimer': 'Swissup_Countdowntimer/js/Swissup/countdowntimer/simpletimer/simpletimer',
-            'starttimers': 'Swissup_Countdowntimer/js/Swissup/countdowntimer/starttimers'
-        }
-    },
-
     shim: {
         'Swissup_Countdowntimer/js/flipclock/flipclock': ['jquery'],
-        'simpletimer': ['jquery']
+        'Swissup_Countdowntimer/js/simpletimer/simpletimer': ['jquery']
     }
-
 };
